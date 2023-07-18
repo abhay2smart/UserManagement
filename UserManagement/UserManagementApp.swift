44eebeb421429ff85176654fc5b595177e0900c7
Abhay <abhayjeetsingh19@gmail.com>
2023-07-18 16:28:09 +0530

    Initial Commit

---
+++ UserManagement/UserManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagementApp.swift
+//  UserManagement
+//
+//  Created by Abhayjeet Singh on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UserManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
